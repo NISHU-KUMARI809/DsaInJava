b8d0b709c6d9ba94b4978c04da8b6b58859ad1a7
Nishu satsangi <nk8686825@gmail.com>
2023-10-20 08:04:20 +0530

    all

--- a6_ArraySecondSmallest.java
+++ a6_ArraySecondSmallest.java
@@ -1,4 +1,21 @@
 public class a6_ArraySecondSmallest {
-    
+    public static void main(String[] args) {
+        int smallest=Integer.MAX_VALUE;
+        int sec_Smallest=smallest;
+        int a[]={12,13,11,122,134,567,1};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i]<smallest){
+                sec_Smallest=smallest;
+                smallest=a[i];
+            }
+            else if(a[i]<sec_Smallest && a[i]<smallest){
+                sec_Smallest=a[i];
+            }
+        }
+        System.out.println("second largest is:"+sec_Smallest);
+    }
     
 }
